a462122711c5e18963f4bb0ec4619708ac47f556
Ashar Shahid <Ashar2shahid@gmail.com>
2025-03-25 11:50:54 +0530

    add mip-m42 validation

--- proposals/mips/mip-m42/mip-m42.sol
+++ proposals/mips/mip-m42/mip-m42.sol
@@ -7,6 +7,8 @@ import {Ownable} from "@openzeppelin-contracts/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 import {ProposalActions} from "@proposals/utils/ProposalActions.sol";
+import {IApi3ReaderProxy} from "@protocol/oracles/IApi3ReaderProxy.sol";
+import {ChainlinkOracle} from "@protocol/oracles/ChainlinkOracle.sol";
 import {MOONBEAM_FORK_ID} from "@utils/ChainIds.sol";
 import {HybridProposal, ActionType} from "@proposals/proposalTypes/HybridProposal.sol";
 import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
@@ -27,28 +29,43 @@ contract mipm42 is HybridProposal {
         return MOONBEAM_FORK_ID;
     }
 
-    function deploy(Addresses addresses, address) public override {
-        if (!addresses.isAddressSet("API3_GLMR_USD_OEV_FEED")) {
-            addresses.addAddress(
-                string(abi.encodePacked("API3_GLMR_USD_OEV_FEED")),
-                address(0x80308dCE5bd550209FDb22871ce411869014EE8E)
-            );
-        }
-    }
-
     function build(Addresses addresses) public override {
         _pushAction(
             addresses.getAddress("CHAINLINK_ORACLE"),
             abi.encodeWithSignature(
                 "setFeed(string,address)",
                 ERC20(addresses.getAddress("mGLMR")).symbol(),
-                addresses.getAddress("API3_GLMR_USD_OEV_FEED")
+                addresses.getAddress("API3_GLMR_USD_FEED")
             ),
-            "Set price feed for WETH"
+            "Set price feed for mGLMR"
         );
     }
 
     function validate(Addresses addresses, address) public override {
         vm.selectFork(primaryForkId());
+
+        ChainlinkOracle oracle = ChainlinkOracle(
+            addresses.getAddress("CHAINLINK_ORACLE")
+        );
+
+        assertEq(
+            oracle.getFeed(ERC20(addresses.getAddress("mGLMR")).symbol()),
+            addresses.getAddress("API3_GLMR_USD_FEED"),
+            "mGLMR feed not set"
+        );
+
+        IApi3ReaderProxy reader = IApi3ReaderProxy(
+            addresses.getAddress("API3_GLMR_USD_FEED")
+        );
+
+        (int256 price, uint256 timestamp) = reader.read();
+
+        assertEq(
+            oracle.getChainlinkPrice(
+                getFeed(ERC20(addresses.getAddress("mGLMR")).symbol())
+            ),
+            uint256(price),
+            "Wrong Price"
+        );
     }
 }
