69e4d76694cd223fb7f4cc89e6441b64c034c03e
Ana Bittencourt <anajuliabit@gmail.com>
2025-05-05 16:57:01 -0300

    add mip o14

---
+++ proposals/mips/mip-o14/o14.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export MIP_REWARDS_PATH=proposals/mips/mip-o14/o14.json
+echo "MIP_REWARDS_PATH=$MIP_REWARDS_PATH"
+
+export DESCRIPTION_PATH=proposals/mips/mip-o14/MIP-O14.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=2
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
+
+
+
